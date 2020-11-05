37b4fd6eb6c7d99b716ace54356423707afd49c9
Deepak Choudhary <choudharydeepak938@gmail.com>
2020-11-05 16:20:16 +0530

    Initial Changes

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,16 @@
 // import using require
-
+const Shape = require("./shape.js");
 // declare class
 
+class Circle extends Shape {
+  constructor() {
+    super();
+  }
+  calculateArea() {
+    console.log("calculate Area of Circle");
+  }
+}
+
+module.exports = Circle;
 
 // export class using module.exports
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,14 @@
 // declare class
-
+class Shape {
+  constructor() {
+    this.color = color;
+  }
+  drawShape() {
+    console.log("draw Shape");
+  }
+  calculateArea() {
+    console.log("calculate Area of Shape");
+  }
+}
 // export class using module.exports
+module.exports = Shape;
